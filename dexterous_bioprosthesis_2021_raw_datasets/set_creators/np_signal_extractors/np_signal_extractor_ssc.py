9fa018895834fc59deff04487bdcdbb9b2ac4436
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2025-11-10 20:48:20 +0100

    1. np_signal_extractor_ssc -- make it a bit faster. 2. version 0.0.16.

--- dexterous_bioprosthesis_2021_raw_datasets/set_creators/np_signal_extractors/np_signal_extractor_ssc.py
+++ dexterous_bioprosthesis_2021_raw_datasets/set_creators/np_signal_extractors/np_signal_extractor_ssc.py
@@ -3,15 +3,6 @@ from dexterous_bioprosthesis_2021_raw_datasets.set_creators.np_signal_extractors
 )
 
 import numpy as np
-from numba import jit
-
-
-# @jit(nopython=True)
-def ssc_int(input):
-    re = np.copy(input).astype(input.dtype)
-    re[re <= 0] = 0.0
-    re[re > 0] = 1.0
-    return re
 
 
 class NpSignalExtractorSsc(NPSignalExtractor):
@@ -20,11 +11,10 @@ class NpSignalExtractorSsc(NPSignalExtractor):
         return super().fit(X)
 
     def _transform(self, X):
-        SSC = np.mean(
-            ssc_int(-np.diff(X, axis=0, prepend=1)[1:-1] * np.diff(X, axis=0)[1:]),
+        return np.mean(
+            ((-np.diff(X, axis=0, prepend=1)[1:-1] * np.diff(X, axis=0)[1:]) > 0).astype(X.dtype),
             axis=0,
         )
-        return SSC.astype(X.dtype)
 
     def attribs_per_column(self):
         return 1
