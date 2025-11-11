c1717c47b3088579a447fb32c819b3d5e41c372a
pawel.trajdos@pwr.edu.pl <pawel.trajdos@pwr.edu.pl>
2025-11-11 13:19:30 +0100

    1. install_libffi -- checks for python version.

--- install_libffi.sh
+++ install_libffi.sh
@@ -2,6 +2,20 @@
 
 # Libffi version and download URL
 VERSION=3.3
+# Get the major and minor version of Python
+PYTHON_VERSION=$(python3 --version 2>&1 | awk '{print $2}')
+PYTHON_MAJOR=$(echo "$PYTHON_VERSION" | cut -d. -f1)
+PYTHON_MINOR=$(echo "$PYTHON_VERSION" | cut -d. -f2)
+
+# Set variable based on version
+if [[ "$PYTHON_MAJOR" -eq 3 && "$PYTHON_MINOR" -eq 9 ]]; then
+    VERSION=3.3
+elif [[ "$PYTHON_MAJOR" -eq 3 && "$PYTHON_MINOR" -eq 11 ]]; then
+    VERSION=3.5.2
+else
+    VERSION=3.3
+fi
+
 URL="https://github.com/libffi/libffi/releases/download/v${VERSION}/libffi-${VERSION}.tar.gz"
 
 
