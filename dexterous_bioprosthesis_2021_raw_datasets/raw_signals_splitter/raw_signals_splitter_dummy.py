96cef7fa43f7e142aeeeaa5cc1f04b85c8ccae4b
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2026-09-16 20:08:56 +0200

    1. raw_signals_splitter_dummy -- now compatible with python 3.9.

--- dexterous_bioprosthesis_2021_raw_datasets/raw_signals_splitter/raw_signals_splitter_dummy.py
+++ dexterous_bioprosthesis_2021_raw_datasets/raw_signals_splitter/raw_signals_splitter_dummy.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from dexterous_bioprosthesis_2021_raw_datasets.raw_signals.raw_signals import RawSignals
 from dexterous_bioprosthesis_2021_raw_datasets.raw_signals_splitter.iraw_signals_splitter import IRawSignalsSplitter
 from copy import deepcopy
@@ -6,6 +6,6 @@ from copy import deepcopy
 class RawSignalsSplitterDummy(IRawSignalsSplitter):
     
 
-    def split_signals(self, raw_signals: RawSignals) -> List[RawSignals] | Tuple[RawSignals]:
+    def split_signals(self, raw_signals: RawSignals) -> Union[List[RawSignals], Tuple[RawSignals]]:
 
         return (deepcopy(raw_signals),)
