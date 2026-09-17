ac089c831b9c8aeebae392c8d3ed52587982f2db
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2026-09-17 11:44:14 +0200

    1. set_creator_concatenated -- small update.

--- dexterous_bioprosthesis_2021_raw_datasets/set_creators/set_creator_concatenated.py
+++ dexterous_bioprosthesis_2021_raw_datasets/set_creators/set_creator_concatenated.py
@@ -18,6 +18,7 @@ class SetCreatorConcatenated(ASetCreator):
 
         self.creators = creators
         self.channel_selected_attribs = None # List containing number of attributes for each channel
+        self._concatenated_channel_names = tuple()
 
     def fit(self, raw_signals: RawSignals, y=None):
         """Fit the transformer to the given data."""
@@ -26,28 +27,42 @@ class SetCreatorConcatenated(ASetCreator):
         for creator in self.creators:
             creator.fit(raw_signals)
 
-        is_creators_channel_attribs = [ True if creator.get_channel_attribs_indices() is not None else False for creator in self.creators ]
+        # Concatenate channel names from all sub-creators
+        self._concatenated_channel_names = []
+        for creator in self.creators:
+            creator_names = creator.get_channel_names()
+            for name in creator_names:
+                if name not in self._concatenated_channel_names:
+                    self._concatenated_channel_names.append(name)
+        self._concatenated_channel_names = tuple(self._concatenated_channel_names)
+
+        # Build concatenated attribute indices
+        is_creators_channel_attribs = [creator.get_channel_attribs_indices() is not None for creator in self.creators]
         if all(is_creators_channel_attribs):
-            self.channel_selected_attribs = None
-            
+            # Build a mapping from channel name to index in the concatenated channel list
+            channel_name_to_idx = {name: idx for idx, name in enumerate(self._concatenated_channel_names)}
+            n_concat_channels = len(self._concatenated_channel_names)
+            self.channel_selected_attribs = [[] for _ in range(n_concat_channels)]
+
+            offset = 0
             for creator in self.creators:
                 creator_channel_attribs = creator.get_channel_attribs_indices()
-                if self.channel_selected_attribs is None:
-                    self.channel_selected_attribs = creator_channel_attribs
-                else:
-                    offset = self._find_offset(self.channel_selected_attribs)
-                    for channel_idx, attrib_indices in enumerate(creator_channel_attribs):
-                        self.channel_selected_attribs[channel_idx] += list(offset +  np.asanyarray(attrib_indices))
+                creator_names = creator.get_channel_names()
+                for creator_ch_idx, attrib_indices in enumerate(creator_channel_attribs):
+                    concat_ch_idx = channel_name_to_idx[creator_names[creator_ch_idx]]
+                    self.channel_selected_attribs[concat_ch_idx] += list(offset + np.asanyarray(attrib_indices))
+                # Compute offset as total number of features from this creator
+                offset += self._count_features(creator_channel_attribs)
 
         return self
+
+    def _count_features(self, channel_attribs):
+        """Count total number of features across all channels."""
+        count = 0
+        for attrib_indices in channel_attribs:
+            count += len(attrib_indices)
+        return count
     
-    def _find_offset(self, channel_selected_attribs):
-        curr_max = 0
-        for li in channel_selected_attribs:
-            tmp_max = np.max(li)
-            if tmp_max > curr_max:
-                curr_max = tmp_max
-        return curr_max + 1
 
     def transform(self, raw_signals: RawSignals):
         """Transform the given data."""
@@ -84,3 +99,7 @@ class SetCreatorConcatenated(ASetCreator):
     def get_channel_attribs_indices(self):
         """Return the feature indices grouped by channel."""
         return self.channel_selected_attribs
+
+    def get_channel_names(self):
+        """Return concatenated channel names from all sub-creators."""
+        return self._concatenated_channel_names
